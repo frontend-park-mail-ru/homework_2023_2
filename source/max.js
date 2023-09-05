9af4672afb3034a25d22559d1da5356b1eb05bba
Glibusss <113942267+Glibusss@users.noreply.github.com>
2023-09-05 23:53:53 +0300

    HW 0 Done. Gleb Makarenko

--- source/max.js
+++ source/max.js
@@ -1,3 +1,16 @@
 'use strict';
 
 const max = numbers => Math.max(...numbers);
+
+const tree = function(height){
+        if (height < 3) return null;
+          
+        let result = '';
+        for (let i = 1; i < Number(height); i++) {
+            let tmp = ''.padStart(Number(height) - i-1, ' ').padEnd(Number(height) + i - 2, '*').padEnd((Number(height)-1) * 2 - 1, ' ');
+            result += tmp + '\n';
+        }
+        result += ' '.repeat(Number(height) - 2) + '|' + ' '.repeat(Number(height) - 2) + '\n';
+        return result;
+        
+    }
