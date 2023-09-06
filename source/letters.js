568de8dd5f764817e02a984fa9bb2e67097cd482
Alexey <ax.chinaev@yandex.ru>
2023-09-06 15:42:04 +0300

    full deleting

--- source/letters.js
+++ source/letters.js
@@ -3,26 +3,28 @@
 // const letters = words => Array.from(words).filter((word, index) => words.indexOf(word) === index).join('');
 
 const removeAll = (words, word) => {
-    let current;
-    while (current = words.indexOf(word) !== -1) {
-        words.splice(current)
+    let current = words.indexOf(word);
+    while (current !== -1) {
+        words.splice(current, 1)
+        current = words.indexOf(word)
     }
+
+    return words
 }
 
 const letters = (words, op = null) => {
-    Array.from(words).forEach((word, index) => {
-        let first_index = words.indexOf(word);
+    words = Array.from(words)
+    let set = new Set();
+    words.forEach(function (word, index, arr) {
+        let first_index = arr.indexOf(word);
         if (first_index !== index) {
-            switch (op) {
-                case null:
-                    removeAll(words, word);
-                    break;
-                case true:
-                    words.splice(index);
-                    break;
-                case false:
-                    words.splice(first_index);
-            }
+            set.add(word)
         }
     })
+    set.forEach(elem => words = removeAll(words, elem))
+
+
+    return words.join('')
 }
+
+console.log(letters('prot otype'))
