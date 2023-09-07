99ff23ee6ea52fdc227297ab59781970255b2c22
Polina <stepolant@gmail.com>
2023-09-07 19:07:22 +0300

    added foreach, arrow function

--- source/format.js
+++ source/format.js
@@ -1,6 +1,6 @@
 'use strict';
 
-const format = function (numbers, columns) {
+const format = (numbers, columns) => {
     if(columns<0){
         return ''
     }
@@ -8,18 +8,18 @@ const format = function (numbers, columns) {
     maxNumInColumns.length=columns;
     maxNumInColumns.fill(0);
     let result = '';
-    for(let i = 0; i<numbers.length; i++){
-        if(String(numbers[i]).length>maxNumInColumns[i%columns]){
-            maxNumInColumns[i%columns] = String(numbers[i]).length;
+    numbers.forEach(function(item, i, numbers) {
+        if(String(item).length>maxNumInColumns[i%columns]){
+            maxNumInColumns[i%columns] = String(item).length;
         }
-    }
-    for(let i = 0; i<numbers.length; i++){
-        result+=' '.repeat(maxNumInColumns[i%columns]-String(numbers[i]).length)+numbers[i];
-        if((i+1)%columns==0){
+    });
+    numbers.forEach(function(item, i, numbers) {
+        result+=' '.repeat(maxNumInColumns[i%columns]-String(item).length)+item;
+        if((i+1)%columns===0){
             result+='\n';
         }else{
             result+=' ';
         }
-    }
+    });
     return result.slice(0, -1);
 };
