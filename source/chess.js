f9e546ef69c03fe14efc4dd42072104b7fcd7e77
George Bezrukov <domnakolesaxxx@gmail.com>
2023-09-08 00:15:54 +0300

    Done hw and tests

---
+++ source/chess.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/**
+ * Функция для получения шахматной доски размером NxN
+ * @param size сторона доски N
+ * @returns {string|null} возвращает доску в виде строки, либо null в случае неверно переданного параметра
+ */
+let chess = size => {
+    if (isNaN(size)) {
+        console.log("Ожидалось число")
+        return null
+    }
+    if (size % 1 != 0) {
+        console.log("Ожидалось целое число")
+        return null
+    }
+    if (size < 2) {
+        console.log("Не бывает доски с таким размером")
+        return null
+    }
+    let desk = ""
+    for (let i = 0; i < size; i++) {
+        for (let j = 0; j < size; j++) {
+            let symb = (i+j) % 2 == 0 ? '*' : ' '
+            desk = desk + symb
+        }
+        desk = desk + '\n'
+    }
+    return desk
+}
--- tests/chess.js
+++ tests/chess.js
@@ -6,6 +6,49 @@ QUnit.module('Тестируем функцию chess', function () {
 		assert.strictEqual(chess('1'), null);
 	});
 
+	QUnit.test('Шахматной доски с отрицательными сторонами не бывает', function (assert) {
+		assert.strictEqual(chess(-10), null);
+		assert.strictEqual(chess('-10'), null);
+	});
+
+	QUnit.test('Шахматной доски с нецелыми сторонами не бывает', function (assert) {
+		assert.strictEqual(chess(1.5), null);
+		assert.strictEqual(chess('1.5'), null);
+	});
+
+	QUnit.test('Нельзя передать слово вместо размера', function (assert) {
+		assert.strictEqual(chess('abcd'), null);
+		assert.strictEqual(chess('10abcd'), null);
+	});
+
+	QUnit.test('Нельзя передать пустую строчку вместо размера', function (assert) {
+		assert.strictEqual(chess(' '), null);
+	});
+
+	QUnit.test('Нельзя передать объект вместо размера', function (assert) {
+		assert.strictEqual(chess(Math), null);
+	});
+
+	QUnit.test('Нельзя передать bool вместо размера', function (assert) {
+		assert.strictEqual(chess(true), null);
+	});
+
+	QUnit.test('Нельзя передать null вместо размера', function (assert) {
+		assert.strictEqual(chess(null), null);
+	});
+
+	QUnit.test('Нельзя передать undefined вместо размера', function (assert) {
+		assert.strictEqual(chess(undefined), null);
+	});
+
+	QUnit.test('Нельзя передать NaN вместо размера', function (assert) {
+		assert.strictEqual(chess(NaN), null);
+	});
+
+	QUnit.test('Шахматной доски бесконечного размера не бывает', function (assert) {
+		assert.strictEqual(chess(Infinity), null);
+	});
+
 	QUnit.test('Шахматная доска 2 на 2', function (assert) {
 		const expected =
 			'* \n' +
