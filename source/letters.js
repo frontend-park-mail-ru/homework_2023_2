741929e3f023c2c3e2d0e89530675ca47576d81d
Alexey <ax.chinaev@yandex.ru>
2023-09-07 17:52:36 +0300

    fix full delete variant

--- source/letters.js
+++ source/letters.js
@@ -1,36 +1,34 @@
 'use strict';
 
-const removeAll = (words, word) => {
-    let current = words.indexOf(word);
-    while (current !== -1) {
-        words.splice(current, 1)
-        current = words.indexOf(word)
-    }
+/**
+ * Возвращает words, удалив все неуникальные символы,
+ *      либо оставив только первое или последнее вхождение.
+ *
+ * @param {string} words строка, из которой удаляются неуникальные символы.
+ * @param {boolean} parameter определяет оставить ли первое
+ *      или последнее вхождение каждого неуникального символа.
+ * @return {string} words, строка, из которой удалены неуникальные символы.
+ */
+const letters = (words, parameter = null) => {
+    let array = Array.from(words)
 
-    return words
-}
-
-const letters = (words, op = null) => {
-    words = Array.from(words)
-
-    switch (op) {
+    switch (parameter) {
         case null:
-            let set = new Set();
-            words.forEach((word, index, arr) => {
-                if (arr.indexOf(word) !== index) set.add(word);
-            })
+            let countEntries = new Map();
+            array.forEach(word => countEntries.has(word) ?
+                countEntries.set(word, countEntries.get(word) + 1) : countEntries.set(word, 1))
 
-            set.forEach(elem => words = removeAll(words, elem))
+            array = array.filter(word => countEntries.get(word) === 1)
             break;
 
         case true:
-            words = words.filter((word, index) => words.indexOf(word) === index);
+            array = array.filter((word, index) => array.indexOf(word) === index);
             break;
 
         case false:
-            words = words.filter((word, index) => words.lastIndexOf(word) === index);
+            array = array.filter((word, index) => array.lastIndexOf(word) === index);
             break;
     }
 
-    return words.join('')
+    return array.join('')
 }
