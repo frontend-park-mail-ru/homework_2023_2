65bb63b16fc2987a292d352a532a6ce17b86ef45
Ruslan Khalikov <rus-h-903@mail.ru>
2023-09-07 22:16:34 +0300

    fix remarks

--- source/solve.js
+++ source/solve.js
@@ -1,6 +1,20 @@
 'use strict';
 
+/**
+ * function that solve arithmetic expression 
+ * @param  {String, Number} args must contain arithmetic expression and value for "x"
+ * @returns {Number} solution of expression
+ */
 const solve = (...args) => {
+    if (args.length != 2) {
+        throw new Error('Error: incorrect number of arguments');
+    }
     let [expression, value] = args;
+    if (typeof expression != 'string' || typeof value != 'number') {
+        throw new Error('Error: incorrect type of arguments');
+    }
+    if (expression.match(/[^-+)(\/\*x\d\s]/)) {
+        throw new Error('Error: found forbidden characters');
+    }
     return eval(expression.replaceAll("x", value));
 };
--- tests/solve.js
+++ tests/solve.js
@@ -10,12 +10,53 @@ QUnit.module('Тестируем функцию solve', function () {
 		assert.strictEqual(solve('((5 - x) * (x + 5)) * x * x', 3), 144);
 	});
 
-	QUnit.test('solve работает как калькулятор при отсутствии x', function (assert) {
-		assert.strictEqual(solve('1 + 1'), 2);
-		assert.strictEqual(solve('2 + 5 - 1'), 6);
-		assert.strictEqual(solve('2 * 5 - 1'), 9);
-		assert.strictEqual(solve('2 * ( 5 - 1 )'), 8);
-		assert.strictEqual(solve('(5 - 3) * (3 + 5)'), 16);
-		assert.strictEqual(solve('((5 - 3) * (3 + 5)) * 3 * 3'), 144);
+	QUnit.test('solve выкидывает ошибку при неправильном ' +
+			   'количестве аргументов', function (assert) {
+		assert.throws(() =>
+			{solve('x + 1'); },
+			new Error('Error: incorrect number of arguments'),
+			'Error thrown'
+		);
+		assert.throws(() =>
+			{solve(); },
+			new Error('Error: incorrect number of arguments'),
+			'Error thrown'
+		);
+		assert.throws(() =>
+			{solve(1, 1, 1); },
+			new Error('Error: incorrect number of arguments'),
+			'Error thrown'
+		);
+	});
+
+	QUnit.test('solve выкидывает ошибку при неправильном ' +
+			   'типе аргументов', function (assert) {
+		assert.throws(() =>
+			{solve(1, 'x + 1'); },
+			new Error('Error: incorrect type of arguments'),
+			'Error thrown'
+		);
+		assert.throws(() =>
+			{solve(1, 1); },
+			new Error('Error: incorrect type of arguments'),
+			'Error thrown'
+		);
+	});
+
+	QUnit.test('solve выкидывает ошибку при невалидных данных', function (assert) {
+		assert.throws(() =>
+			{solve('y + 1', 1); },
+			new Error('Error: found forbidden characters'),
+			'Error thrown'
+		);
+	});
+
+	QUnit.test('solve выкидывает ошибку при попытке вызвать функцию, ' +
+			   'отличную от арифметических операторов', function (assert) {
+		assert.throws(() =>
+			{solve('alert("1")', 1); },
+			new Error('Error: found forbidden characters'),
+			'Error thrown'
+		);
 	});
 });
