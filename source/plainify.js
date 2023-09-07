7d1c0ade0eb953e6a2d5d634c57d1b61a025731c
Gvidow <arzamachev.art-alex@yandex.ru>
2023-09-08 01:38:40 +0300

    rewrite the function rec_build with the used reduce

--- source/plainify.js
+++ source/plainify.js
@@ -1,16 +1,14 @@
 'use strict';
 
+const rec_build = (obj, prefix) => {
+    return Object.entries(obj).reduce((res_obj, [key, val]) => {
+        return {...res_obj, ...(val instanceof Object ? rec_build(val, prefix + key + '.') : {[prefix + key]: val})}
+    }, {});
+}
+
 const plainify = object => {
-    let res = {};
-    const rec_build = (obj, prefix) => {
-        for (let prop in obj) {
-            if (obj[prop] instanceof Object) {
-                rec_build(obj[prop], prefix + prop + '.');
-            } else {
-                res[prefix + prop] = obj[prop];
-            }
-        }
+    if (!(object instanceof Object)) {
+        throw new TypeError('expected Object');
     }
-    rec_build(object, '');
-    return res;
+    return rec_build(object, '');
 }
