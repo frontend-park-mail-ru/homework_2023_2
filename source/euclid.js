73494ce52bbba91b3a68191df933ca98ec854dff
Islam <109366718+JuFnd@users.noreply.github.com>
2023-09-07 22:49:22 +0300

    Add description, some tests and reduce fix

--- source/euclid.js
+++ source/euclid.js
@@ -1,23 +1,36 @@
 'use strict';
 
+/**
+ * Вычисляет наибольший общий делитель для переданных аргументов.
+ * @param {...number} args - Числа, для которых нужно вычислить наибольший общий делитель.
+ * @returns {number|null} - Наибольший общий делитель переданных чисел или null, если переданы некорректные аргументы.
+ */
 const euclid = (...args) => {
-  if (args.length === 0) {
+  const isValid = args.every(arg => typeof arg === 'number' && !isNaN(arg));
+  
+  if (!isValid) {
     return null;
   }
   
-  let gcd = args[0];
-  
-  args.slice(1).forEach((num) => {
-    gcd = calculateGCD(gcd, num);
-  });
+  if (args.length === 0) {
+    return null;
+  }
+
+  const gcd = args.slice(1).reduce((acc, num) => calculateGCD(acc, num), args[0]);
   
   return Math.abs(gcd);
-}
-  
+};
+
+/**
+* Вычисляет наибольший общий делитель для двух чисел.
+* @param {number} a - Первое число.
+* @param {number} b - Второе число.
+* @returns {number} - Наибольший общий делитель для двух чисел.
+ */
 const calculateGCD = (a, b) => {
   while (b !== 0) {
     [a, b] = [b, a % b];
   }
-  
+    
   return a;
-}
+};
--- tests/euclid.js
+++ tests/euclid.js
@@ -55,4 +55,18 @@ QUnit.module('Тестируем функцию euclid', function () {
     assert.strictEqual(euclid(13, 13, 13), 13, 'euclid(13, 13, 13) === 13');
     assert.strictEqual(euclid(2, 2, 2, 2), 2, 'euclid(2, 2, 2, 2) === 2');
   });
+
+  QUnit.test('Функция должна правильно находить НОД десятичных дробей(доп)', function (assert) {
+    assert.strictEqual(euclid(1.5, 0.5), 0.5, 'euclid(1.5, 0.5) === 0.5');
+    assert.strictEqual(euclid(0.3, 0.6), 0.3, 'euclid(0.3, 0.6) === 0.3');
+    assert.strictEqual(euclid(2.5, 1.5, 7.5, 8.5), 0.5, 'euclid(2.5, 1.5, 7.5, 8.5) === 0.5');
+    assert.strictEqual(euclid(0.25, 0.5, 0.75), 0.25, 'euclid(0.25, 0.5, 0.75) === 0.25');
+  });
+
+  QUnit.test('Функция должна правильно обрабатывать некорректные данные(доп)', function (assert) {
+    assert.strictEqual(euclid(1, 1.5, "abc"), null, 'euclid(1, 1.5, "abc") должна вернуть null');
+    assert.strictEqual(euclid(1, "123", "abc"), null, 'euclid(1, "123", "abc") должна вернуть null');
+    assert.strictEqual(euclid(1, "123", 1), null, 'euclid(1, 1.5, 1) должна вернуть null');
+  });
+
 });
