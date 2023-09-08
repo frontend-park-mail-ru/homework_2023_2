cfc885b5c54efd112712b2a8b772d5ece49c0022
Gvidow <arzamachev.art-alex@yandex.ru>
2023-09-08 17:42:17 +0300

    add a JSDoc tag to describe for my functions

--- source/plainify.js
+++ source/plainify.js
@@ -1,11 +1,21 @@
 'use strict';
 
+/**
+ * The function of adding a prefix to the properties of an object.
+ * @param {Object} obj - Source object.
+ * @param {string} prefix - Prefix to add to object properties.
+ */
 const rec_build = (obj, prefix) => {
     return Object.entries(obj).reduce((res_obj, [key, val]) => {
         return {...res_obj, ...(val instanceof Object ? rec_build(val, prefix + key + '.') : {[prefix + key]: val})}
     }, {});
 }
 
+
+/**
+ * Converting an object to plain.
+ * @param {Object} object - Source object.
+ */
 const plainify = object => {
     if (!(object instanceof Object)) {
         throw new TypeError('expected Object');
