90bce9790370fb14de6f285f5e62ff61a809db19
Alexander Tarba <sachatarba@rambler.ru>
2023-09-08 13:14:11 +0300

    some fixes

--- source/plain.js
+++ source/plain.js
@@ -1,19 +1,25 @@
 'use strict';
 
+/**
+ * @param {Array} array - Массив элементов
+ * @description Функция рекурсивно обходит вложенные массивы, добавляя их элементы в результирующий одномерный массив
+ * @returns {Array} Результирующий одномерный массив, содержащий в себе все элементы вложенных
+ */
 const plain = function (array) {
     if (!(array instanceof Array)) {
         return array;
     }
 
-    let res = [];
+    const res = [];
 
-    for (let elm of array) {
+    array.forEach((elm) => {
         if (elm instanceof Array) {
             res.push(...plain(elm));
         } else {
             res.push(elm);
         }
-    }
+    })
 
     return res;
 };
+
