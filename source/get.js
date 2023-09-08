4e9317cbab0f05400f38219349f0a2f50c9c5467
Kayetana <Ane4kaGr@mail.ru>
2023-09-08 14:04:50 +0300

    implement logic for getting property value

--- source/get.js
+++ source/get.js
@@ -4,3 +4,13 @@
     Напишите функцию `get`, которая получает путь к вложенному свойству объекта
     и возвращает значение этого свойства (или undefined, если свойства не существует)
 */
+
+let get = (object, pathToProperty) => {
+    if (pathToProperty === '.') {
+        return object;
+    }
+
+    let propertyNames = pathToProperty.slice(1).split('.');
+    let propertyValue = propertyNames.reduce((previous, propertyName) => previous[propertyName], object);
+    return propertyValue;
+};
