590756c40e92ab027b8388fefa708a6f8b93ad58
Kayetana <Ane4kaGr@mail.ru>
2023-09-08 23:50:59 +0300

    replace magic number with uppercase constant

--- source/get.js
+++ source/get.js
@@ -5,12 +5,14 @@
     и возвращает значение этого свойства (или undefined, если свойства не существует)
 */
 
+const PATH_START_INDEX = 1;
+
 const get = (object, pathToProperty) => {
     if (pathToProperty === '.') {
         return object;
     }
 
-    const propertyNames = pathToProperty.slice(1).split('.');
+    const propertyNames = pathToProperty.slice(PATH_START_INDEX).split('.');
 
     return propertyNames.reduce((previous, propertyName) =>
         previous === undefined ? undefined : previous[propertyName], object);
