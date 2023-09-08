01fa54de78d2666531039f134896a1acd4e8187c
wizardOfOz21 <gasaevgamid@gmail.com>
2023-09-08 19:33:51 +0300

    Исправление документации функции inverse

--- source/inverse.js
+++ source/inverse.js
@@ -6,13 +6,13 @@
  * @param {number} offsetIndex - Количество элементов, остающихся на месте:
  * при положительном значении - с начала массива, при отрицательном - с конца.
  * @returns {Array} - Массив с обратным порядком элементов с учетом offsetIndex.
- * @throws - Бросает ошибку типа, если первым аргументом передан не массив.
- * @throws - Бросает ошибку типа, если вторым аргументом передано не целое число.
+ * @throws {TypeError} - Если первым аргументом передан не массив 
+ * или вторым - не целое число.
  */
 const inverse = function (array, offsetIndex = 0) {
     if (!Array.isArray(array)) throw new TypeError("Arg1 is not an Array"); 
     if (!Number.isInteger(offsetIndex)) throw new TypeError("Arg2 is not an Integer");
     if (offsetIndex == 0) return array.reverse(); 
-    return offsetIndex >= 0 ? array.slice(0, offsetIndex).concat(array.slice(offsetIndex).reverse()) :
+    return offsetIndex > 0 ? array.slice(0, offsetIndex).concat(array.slice(offsetIndex).reverse()) :
         array.slice(0, offsetIndex).reverse().concat(array.slice(offsetIndex));
 };
