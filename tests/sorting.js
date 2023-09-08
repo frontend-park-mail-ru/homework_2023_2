4a1cbf68ae1bfdb8ea6cdd3fab4c469cc5c91e3c
Gallaann <reindzer26rus@gmail.com>
2023-09-08 03:09:06 +0300

    Add own tests

--- tests/sorting.js
+++ tests/sorting.js
@@ -29,6 +29,25 @@ QUnit.module('Тестируем функцию sorting', function () {
 		assert.deepEqual(actual, expected);
 	});
 
+	QUnit.test('sorting не меняет массив, если передано несуществующее поле для сортировки', function (assert) {
+		const initial = [
+			{prop1: 1},
+			{prop1: 2},
+			{prop1: 3},
+			{prop1: 4}
+		];
+		const actual = sorting(initial, [ 'prop2' ]);
+
+		const expected = [
+			{prop1: 1},
+			{prop1: 2},
+			{prop1: 3},
+			{prop1: 4}
+		];
+
+		assert.deepEqual(actual, expected);
+	});
+
 	QUnit.test('sorting сортирует массив по численному свойству', function (assert) {
 		const initial = [
 			{prop1: 30},
@@ -116,6 +135,86 @@ QUnit.module('Тестируем функцию sorting', function () {
 			{prop1: 2, id: '2'},
 			{prop1: 3, id: '2'},
 			{prop1: 4, id: '2'}
+		];
+
+		assert.deepEqual(actual, expected);
+	});
+
+	QUnit.test('sorting не изменяет корректно отсортированный массив', function (assert) {
+		const initial = [
+			{prop1: 30, prop2: 'a'},
+			{prop1: 1000, prop2: 'b'},
+			{prop1: 4, prop2: 'c'},
+			{prop1: 200, prop2: 'd'}
+		];
+		const actual = sorting(initial, [ 'prop2', 'prop1' ]);
+
+		const expected = [
+			{prop1: 30, prop2: 'a'},
+			{prop1: 1000, prop2: 'b'},
+			{prop1: 4, prop2: 'c'},
+			{prop1: 200, prop2: 'd'}
+		];
+
+		assert.deepEqual(actual, expected);
+	});
+
+	QUnit.test('sorting сортирует массив по численному свойству и строковому свойству, содержащий одинаковые значения', function (assert) {
+		const initial = [
+			{prop1: 30, prop2: 'a'},
+			{prop1: 1000, prop2: 'b'},
+			{prop1: 4, prop2: 'c'},
+			{prop1: 200, prop2: 'd'},
+			{prop1: 30, prop2: 'c'},
+			{prop1: 4, prop2: 'a'},
+		];
+		const actual = sorting(initial, [ 'prop2', 'prop1' ]);
+
+		const expected = [
+			{prop1: 4, prop2: 'a'},
+			{prop1: 30, prop2: 'a'},
+			{prop1: 1000, prop2: 'b'},
+			{prop1: 4, prop2: 'c'},
+			{prop1: 30, prop2: 'c'},
+			{prop1: 200, prop2: 'd'}
+		];
+
+		assert.deepEqual(actual, expected);
+	});
+
+	QUnit.test('sorting сортирует массив по нескольким свойствам, одно из которых отсутствует в объекте', function (assert) {
+		const initial = [
+			{prop1: 3, prop2: 'b'},
+			{prop1: 2, prop2: 'a'},
+			{prop1: 1, prop2: 'c'},
+			{prop1: 4},
+		];
+		const actual = sorting(initial, [ 'prop2', 'prop1' ]);
+
+		const expected = [
+			{prop1: 2, prop2: 'a'},
+			{prop1: 3, prop2: 'b'},
+			{prop1: 1, prop2: 'c'},
+			{prop1: 4},
+		];
+
+		assert.deepEqual(actual, expected);
+	});
+
+	QUnit.test('sorting сортирует массив по свойству, которое содержит значение null', function (assert) {
+		const initial = [
+			{prop1: 3, prop2: null},
+			{prop1: 2, prop2: 'a'},
+			{prop1: 1, prop2: 'c'},
+			{prop1: 4},
+		];
+		const actual = sorting(initial, [ 'prop2', 'prop1' ]);
+
+		const expected = [
+			{prop1: 2, prop2: 'a'},
+			{prop1: 1, prop2: 'c'},
+			{prop1: 3, prop2: null},
+			{prop1: 4},
 		];
 
 		assert.deepEqual(actual, expected);
