5772ccb09066a7830fb38b7ba1c769b0fc45e53b
1qwaka <yalenin01@gmail.com>
2023-09-09 23:14:19 +0300

    minmax fix & jsdoc added

--- source/minmax.js
+++ source/minmax.js
@@ -1,15 +1,32 @@
 'use strict';
 
+/**
+ * 
+ * @param {String} str строка, в которой содержатся числа (могут вперемешку с любями другими символами)
+ * @return {Number[]} массив из двух чисел, в котором нулевой элемент - минимальное число из стороки, 
+ *                    первый элемент - максимальное 
+ */
 function minmax(str) {
-    const numbers = str.split(/[^\w\-.а-яА-Я]+/gu)
-        .filter(s => s.length > 0)
-        .map(s => Number(s))
-        .filter(num => !Number.isNaN(num));
+    const numberStrings = str.match(/[+-]?((\d+([.]\d*)?([eE][+-]?\d+)?|[.]\d+([eE][+-]?\d+)?)|Infinity)/gu);
+    if (!numberStrings) {
+        return [ undefined, undefined ];
+    }
+
+    const numbers = [];
+    for (const numStr of numberStrings) {
+        if (numStr.length > 0) {
+            const num = Number(numStr);
+            if (!Number.isNaN(num)) {
+                numbers.push(num);
+            }
+        }
+    }
 
     const minAndMax = [ undefined, undefined ];
 
-    if (numbers.length == 0)
+    if (numbers.length === 0) {
         return minAndMax;
+    }
 
     minAndMax[0] = minAndMax[1] = numbers[0];
 
