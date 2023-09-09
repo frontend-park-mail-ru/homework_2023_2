2bb3011953b24ade54f6eb78322996134d424751
almkuznetsov <almkuznetsov@mail.ru>
2023-09-09 13:17:02 +0300

    docs deleted, makeLine func added, fixes added

--- source/chess.js
+++ source/chess.js
@@ -1,26 +1,39 @@
-//'use strict';
-/**
-Функция рисует рисует ASCII-шахматрую доску размером N*N символов из звёздочек 
-(в левом верхнем углу всегда стоит звёздочка)
-@param {number} size - размер доски
-*/
+'use strict';
+
 const BLACK_FIELD = ' ';
 const WHITE_FIELD = '*'
+
+/**
+ Функция генерирует строку шахматной доски
+ @param {number} size - длина строки
+ @param {string} firstField - первый элемент строки, может быть BLACK_FIELD или WHITE_FIELD в зависимости от того, рисуем четную или нечетную строку
+ @returns {string} - строка шахматной доски
+ */
+const makeLine = (size, firstField) => {
+    let line = Array.from({length: size}, (size, i) => {
+        return (i++ % 2) ? WHITE_FIELD : BLACK_FIELD
+    })
+    if (firstField === WHITE_FIELD) { line.unshift(WHITE_FIELD); line.pop(); };
+    line.push('\n');
+    return line.join("");
+}
+
+/**
+ Функция рисует рисует ASCII-шахматрую доску размером N*N символов из звёздочек
+ (в левом верхнем углу всегда стоит звёздочка)
+ @param {number} size - размер доски
+ @returns {string} - строка, изображающая шахматную доску
+ */
 const chess = (size) => {
-    if (size <= 1) return null;
+    if (size <= 1) { return null };
     let field = '';
-    let i = 0;
-    oddLine = Array.from({ length : size }, (size, i) => { if (i++ % 2) return WHITE_FIELD; else return BLACK_FIELD; } )
-    evenLine = Array.from({ length : size }, (size, i) => { if (i++ % 2) return BLACK_FIELD; else return WHITE_FIELD; } )
-    oddLine.push('\n');
-    evenLine.push('\n');
+    let oddLine = makeLine(size, BLACK_FIELD)
+    let evenLine = makeLine(size, WHITE_FIELD)
+
     for (let i = 1; i <= size; i++) {
-        if (i % 2) { 
-            field += evenLine.join(""); 
-        }
-        else {
-            field += oddLine.join("");
-        }
+        (i % 2)
+            ? (field += evenLine)
+            : (field += oddLine)
     }
     return field;
 };
