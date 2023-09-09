a74d2413232d8b14bf4ab77d7fccfe5ab5d87aae
dima <dima>
2023-09-09 19:30:37 +0300

    remove magic constant and extra func

--- source/get.js
+++ source/get.js
@@ -1,36 +1,30 @@
 'use strict';
 
 /**
- * @param {string} object
- * @param {Array} properties
- * @returns {undefined|*}
- * @description Рекурсивная вспомогательная функция для получения свойств объекта
- */
-function getImpl(object, properties) {
-    if (!properties.length) {
-        return object
-    }
-
-    if (object[properties[0]] !== undefined) {
-        return getImpl(object[properties[0]], properties.slice(1))
-    }
-
-    return undefined;
-}
-/**
  * @param {object} object Объект, свойство которого мы хотим получить
  * @param {string} query Dot notation запрос для получения свойства некоторой вложенности
  * @returns {*|undefined} Свойство, другой вложенный объект, сам объект(при query === '.'), undefined, если такого свойства нет
  * @description Возвращает свойтсво объекта(в т.ч. вложенное) при его наличии.
  * В ином случае возвращает undefined
  */
+
+const MIN_ARGS_COUNT = 2;
+const QUERY_WITHOUT_DOT = 1;
+const DOT_SYMBOL = '.';
 function get(object, query) {
-    if (arguments.length < 2 || query == undefined) {
+    if (arguments.length < MIN_ARGS_COUNT || query === undefined || object === undefined) {
         return undefined;
     }
-    if (query === '.') {
+    if (query === DOT_SYMBOL) {
         return object;
     }
-    const properties = query.slice(1).split('.');
-    return getImpl(object, properties);
+
+    const properties = query.slice(QUERY_WITHOUT_DOT).split(DOT_SYMBOL);
+    for (const index in properties) {
+        object = object[properties[index]];
+        if (object === undefined) {
+            break;
+        }
+    }
+    return object;
 }
