0f8d8540de741135f711f418419800c06f4b35f6
Dmitriy Ostrometskiy <ostrometskiy@internet.ru>
2023-09-09 19:09:57 +0300

    rewrite filter on slice

--- source/filter.js
+++ source/filter.js
@@ -15,23 +15,17 @@ const filter = (text, tags = []) => {
     if (!Array.isArray(tags)){
         throw new Error("expected Array");
     }
-
-    const symbolPoses =  [...text.matchAll(/[&"<>']/g)];
-    let resArr = [];
-    let prevCut = 0;
-    for(let i = 0; i < symbolPoses.length; i++){
-        if(symbolPoses[i] == "<" && symbolPoses[i+1] == ">"){
-            let posibleTag = text.slice(symbolPoses[i].index + 1, symbolPoses[i+1].index);
-            if( posibleTag.startsWith("/"))
-                posibleTag = posibleTag.slice(1);
-            if(tags.includes(posibleTag)){
-                i++;
+    let resArr = text.split(/([&"<>'])/g);
+    for(let i = 0; i < resArr.length; i++){
+        if(map[resArr[i]] !== undefined){
+            if( resArr[i] == "<" && resArr[i+2] == ">" &&
+                tags.includes(resArr[i+1].startsWith("/") ? resArr[i+1].slice(1) : resArr[i+1])
+            ) {
+                i+= 2;
                 continue;
             }
+            resArr[i] = map[resArr[i]];
         }
-        resArr.push(text.slice(prevCut, symbolPoses[i].index) + map[symbolPoses[i]]);
-        prevCut = symbolPoses[i].index + 1;
     }
-    resArr.push(text.slice(prevCut));
     return resArr.join("");
 }
--- tests/filter.js
+++ tests/filter.js
@@ -26,11 +26,25 @@ QUnit.module('Проверка работы функции filter', function () 
 		assert.strictEqual(filter(`<img src="bad" onerror="alert('1');">`, [ 'strong', 'em' ]), '&lt;img src=&quot;bad&quot; onerror=&quot;alert(&#39;1&#39;);&quot;&gt;');
 	});
 
+	QUnit.test('filter экранирует символы подряд, в конце, в начале', function (assert) {
+		assert.strictEqual(filter(`"1 && 1 <script>alert('1');</script>"`, [ 'script' ]), '&quot;1 &amp;&amp; 1 <script>alert(&#39;1&#39;);</script>&quot;');
+	});
+
+	QUnit.test('filter экранирует с одним аргументом', function (assert) {
+		assert.strictEqual(filter(`<script>alert('1');</script>`), '&lt;script&gt;alert(&#39;1&#39;);&lt;/script&gt;');
+		assert.strictEqual(filter(`<img src="bad" onerror="alert('1');">`), '&lt;img src=&quot;bad&quot; onerror=&quot;alert(&#39;1&#39;);&quot;&gt;');
+	});
+
 	QUnit.test('filter допускает только массив тегов', function (assert) {
 		assert.throws(()=> filter(`<script>alert('1');</script>`, null), new Error("expected Array"));
 	});
 
-	QUnit.test('filter обрабатывает символы подряд, в конце, в начале', function (assert) {
-		assert.strictEqual(filter(`"1 && 1 <script>alert('1');</script>"`, [ 'script' ]), '&quot;1 &amp;&amp; 1 <script>alert(&#39;1&#39;);</script>&quot;');
+
+	QUnit.test('filter допускает только текст', function (assert) {
+		assert.throws(()=> filter([1, 2 , 4]), new Error("expected String"));
 	});
+
+
+
+
 });
