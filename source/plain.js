99eb070272671e5d5eeb99071dc74cc824b67f02
wonderf00l <maks22036@gmail.com>
2023-09-09 18:33:13 +0300

    переписал с использованием reduce(), добавил проверку аругмента на входе

--- source/plain.js
+++ source/plain.js
@@ -1,11 +1,22 @@
 'use strict';
 
 /**
- * @param {Array} nestedArray Массив массивов, входящие массивы могут быть любой вложенности.
+ * @param {Array} initialArray Массив массивов, входящие массивы могут быть любой вложенности.
  * @return {Array} unpackedArray Результирующий массив, включающий все элементы исходного.
  */
 const plain = function (nestedArray) {
-    let unpackedArray = []
-    nestedArray.forEach(arrayItem => Array.isArray(arrayItem) ? unpackedArray = unpackedArray.concat(plain(arrayItem)) : unpackedArray.push(arrayItem))
-    return unpackedArray
-};
+    console.log(nestedArray)
+    if (Array.isArray(nestedArray)) {
+        const plainInner = (nestedArray) => nestedArray.reduce((unpackedArray, arrayValue) => unpackedArray.concat(Array.isArray(arrayValue) ? plainInner(arrayValue) : arrayValue), [])
+        return plainInner(nestedArray)
+    }
+    else {
+        return []
+    }
+}
+
+// const plain = function (nestedArray) {
+//     let unpackedArray = []
+//     nestedArray.forEach(arrayItem => Array.isArray(arrayItem) ? unpackedArray = unpackedArray.concat(plain(arrayItem)) : unpackedArray.push(arrayItem))
+//     return unpackedArray
+// };
