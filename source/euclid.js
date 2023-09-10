3801470751c59d6ceafa80201d79086baa207a32
Alex <Alex.v.danilchenko@yandex.ru>
2023-09-11 00:54:19 +0300

    gcd rebase

--- source/euclid.js
+++ source/euclid.js
@@ -1,17 +1,6 @@
 'use strict';
 
-const gcd = (x, y) => {
-	while (x > 0 & y > 0) {
-		if (x > y) {
-			x = x % y;
-		}
-		else {
-			y = y % x;
-		}
-	}
-	
-	return x + y;
-}
+const gcd = (x, y) => y == 0 ? x : gcd(y, 	x % y)
 
 const euclid = (...numbers) => {
 	if (numbers.some((number) => !Number.isFinite(number) ? true : false)) {
