dcd7733d77da28eb4c1d1c776998dbdcf1f466e2
Alex <Alex.v.danilchenko@yandex.ru>
2023-09-11 01:49:13 +0300

    JSDoc 1

--- source/euclid.js
+++ source/euclid.js
@@ -1,11 +1,24 @@
 'use strict';
 
+/**
+ * Нахождение НОД для двух чисел
+ *
+ * @param  {number} x, y - два числа, для которых ищется НОД
+ * @returns {number} x - НОД для двух чисел
+ */
 const gcd = (x, y) => y ? gcd(y, x % y) : x
 
+/**
+ * Нахождение НОД для любого количества чисел
+ *
+ * @param {numbers} numbers - любое количество чисел
+ * @returns {number} НОД для любого количества чисел
+ *
+*/
 const euclid = (...numbers) => {
 	if (numbers.some((number) => !Number.isInteger(number))) {
 		return null;
 	}		
 	
-	return numbers.reduce((previousValue, current) => gcd(previousValue, Math.abs(current)), 0);
+	return numbers.map(Math.abs).reduce(gcd, 0);
 }
