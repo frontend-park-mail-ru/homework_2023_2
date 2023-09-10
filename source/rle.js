ceac9daac6f4a928ff57d53f4ae712ea37e72694
SmileyTheSmile <34063023+SmileyTheSmile@users.noreply.github.com>
2023-09-10 23:36:09 +0300

    Changed behavior when passing a wrong type and added semicolons

--- source/rle.js
+++ source/rle.js
@@ -8,14 +8,14 @@
  */
 const rle = function (stringToCompress) {
     // Проверка на тип, единственный вариант который работал
-    if (typeof stringToCompress !== 'string' && !(stringToCompress instanceof String)){
-        return null
+    if (typeof stringToCompress !== 'string' && !(stringToCompress instanceof String)) {
+        throw new TypeError("Введенный тип не является строкой.");
     }
     // RegExpr, обозначающее все группы повторяющихся символов
-    const regex = new RegExp(/(.)\1+/, 'g')
+    const regex = new RegExp(/(.)\1+/, 'g');
 
     // Функция, заменяющая найденные группы их первым символом и длиной группы
-    const replacementFunc = (match, symbol1) => symbol1 + match.length
+    const replacementFunc = (match, symbol1) => symbol1 + match.length;
 
     return stringToCompress.replaceAll(regex, replacementFunc);
 }
--- tests/rle.js
+++ tests/rle.js
@@ -7,9 +7,6 @@ QUnit.module('Тестируем функцию rle', function () {
 	QUnit.test('Много групп', function (assert) {
 		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
 	});
-	QUnit.test('Не тот тип', function (assert) {
-		assert.strictEqual(rle(10087), null);
-	});
 	QUnit.test('Нет повторений', function (assert) {
 		assert.strictEqual(rle('ABC'), 'ABC');
 	});
@@ -22,15 +19,6 @@ QUnit.module('Тестируем функцию rle', function () {
 	QUnit.test('Не Цифры', function (assert) {
 		assert.strictEqual(rle('....---'), '.4-3');
 	});
-	QUnit.test('null', function (assert) {
-		assert.strictEqual(rle(null), null);
-	});
-	QUnit.test('inf', function (assert) {
-		assert.strictEqual(rle(Infinity), null);
-	});
-	QUnit.test('undefined', function (assert) {
-		assert.strictEqual(rle(undefined), null);
-	});
 	QUnit.test('new String()', function (assert) {
 		assert.strictEqual(rle(new String("ADDD")), "AD3");
 	});
@@ -39,5 +27,17 @@ QUnit.module('Тестируем функцию rle', function () {
 	});
 	QUnit.test('Пустая строка new String()', function (assert) {
 		assert.strictEqual(rle(new String()), "");
+	});
+	QUnit.test('null', function (assert) {
+		assert.throws(() => rle(null), TypeError);
+	});
+	QUnit.test('inf', function (assert) {
+		assert.throws(() => rle(Infinity), TypeError);
+	});
+	QUnit.test('undefined', function (assert) {
+		assert.throws(() => rle(undefined), TypeError);
+	});
+	QUnit.test('Не тот тип', function (assert) {
+		assert.throws(() => rle(10087), TypeError);
 	});
 });
