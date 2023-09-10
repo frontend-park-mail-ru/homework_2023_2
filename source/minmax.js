4ebb3bedcb592754965354058511d4aaf331a0b3
nikita <nikita.borisenckow@yandex.com>
2023-09-10 15:18:05 +0300

    JSDoc was added. Foreash was changed to reduce func, small codestyle fixes

--- source/minmax.js
+++ source/minmax.js
@@ -1,34 +1,40 @@
 "use strict";
 
+/**
+ * Эта функция возвращает массив чисел из переданной ей строки, игнорируя текст.
+ * @param {str} str - Строка
+ * @returns {arrToRet[]} - массив чисел Float
+ * */
 const getNumbersArr = (str) => {
-  let arrToRet = [];
-
   const strs = str.split(" ");
-  strs.forEach((el) => {
-    const tmp = parseFloat(el);
-    if (!isNaN(tmp)) {
-      arrToRet.push(tmp);
-    }
-  });
+  const arrToRet = strs.map((el) => parseFloat(el)).filter((el) => !isNaN(el));
   return arrToRet;
 };
 
+/**
+ * Функция находит минимальное и максимальное число в строке чисел.
+ *
+ * @param {string} numbers - Строка чисел, разделенных пробелами.
+ * @returns {number[]} Массив, с минимальным и максимальным числами.
+ * Если строка не содержит чисел - возвращается [undefined, undefined].
+ */
 const minmax = (numbers) => {
-  if (typeof numbers === "string") {
-    const numbersArr = getNumbersArr(numbers);
-
-    if (numbersArr.length === 0) {
-      return [undefined, undefined];
-    }
-
-    let min = Number.POSITIVE_INFINITY;
-    let max = Number.NEGATIVE_INFINITY;
-
-    numbersArr.forEach((element) => {
-      min = element < min ? element : min;
-      max = element > max ? element : max;
-    });
-
-    return [min, max];
+  if (typeof numbers != "string") {
+    return [undefined, undefined];  
   }
+
+  const numbersArr = getNumbersArr(numbers);
+  if (numbersArr.length === 0) {
+    return [undefined, undefined];
+  }
+
+  const result = numbersArr.reduce(
+    (acc, curVal) => {
+      acc[0] = curVal < acc[0] ? curVal : acc[0];
+      acc[1] = curVal > acc[1] ? curVal : acc[1];
+      return acc;
+    }, [Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY]);
+
+  return result;
+  
 };
