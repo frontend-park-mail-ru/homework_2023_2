136cefb9d6b1f2ebbe9f393b7a077a58f289f41f
alfa <alfaiv@yandex.ru>
2023-09-10 18:34:59 +0300

    second rev

--- source/solve.js
+++ source/solve.js
@@ -1,16 +1,21 @@
 'use strict'
 
+/**
+ * @param {string} equation - Исходное выражение
+ * @param {number} x_value - Значение для подстановки в x
+ * @description Подставновка в выражение с x, соответсвующего значени
+ * @returns {number} Вычисленное значение после подставновки x
+ */
+
+
 const solve = function (equation, x_value) {
     
     let solution;
     let char;
-    // console.log(equation);
 
     for (let i = 0; i < equation.length; i++) {
         char = equation[i];
-        // console.log(char);
-        if (char == "x"){
-            // console.log(i);
+        if (char === "x"){
             equation = equation.replace(equation[i], String(x_value));;
         }
     }
@@ -22,5 +27,6 @@ const solve = function (equation, x_value) {
 
     } catch {
         console.log("Неверно введено уравнение");
+        throw new Error('Error: incorrect input data');
     }
 };
