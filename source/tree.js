4f9a76085414683ee804ad9f94dfae89f4a23aff
Khristina <hristinakirillova2559@gmail.com>
2023-09-11 01:30:13 +0300

    no comments + new level scope

--- source/tree.js
+++ source/tree.js
@@ -1,9 +1,5 @@
 'use strict';
 
-const level = (strings, character, amountOfChar, amountOfSpace) => {
-    return " ".repeat(amountOfSpace) + `${character}`.repeat(amountOfChar) + " ".repeat(amountOfSpace);
-}
-
 /**
  * function create a tree 
  * @param  {(Number|String), (Number|String)} arg contain height of the tree and height of the trunk
@@ -12,6 +8,11 @@ const level = (strings, character, amountOfChar, amountOfSpace) => {
  */
 
 const tree = (height, trunkHeight = 1) => {
+
+    const level = (strings, character, amountOfChar, amountOfSpace) => {
+        return " ".repeat(amountOfSpace) + `${character}`.repeat(amountOfChar) + " ".repeat(amountOfSpace);
+    }
+
     if (isNaN(height) || height === Infinity || height === "") {
         throw new Error("Error: invalid value of height");
     }
@@ -24,16 +25,16 @@ const tree = (height, trunkHeight = 1) => {
         throw new Error("Error: invalid value of trunk height");
     }
 
-    const trunkWidth = 1; //ширина ствола
-    const baseOfTree = 2 * (height - trunkHeight) - 1; //кол-во звездочек на нижнем уровне елки, так как на n уровне 2 * n - 1 число звездочек
+    const trunkWidth = 1;
+    const baseOfTree = 2 * (height - trunkHeight) - 1;
 
     let result = "";
     for(let i = 0; i < trunkHeight; ++i) {
-        result = `${level `${"|"}${trunkWidth}${(baseOfTree - trunkWidth) / 2}`}\n${result}`; //(baseOfTree - trunkWidth) / 2 - кол-во пробелов перед и после ствола
+        result = `${level `${"|"}${trunkWidth}${(baseOfTree - trunkWidth) / 2}`}\n${result}`;
     }
 
-    for(let padding = 0; padding < height - trunkHeight; ++padding) { //padding - кол-во пробелов перед и после звездочек
-        result = `${level `${"*"}${baseOfTree - 2 * padding}${padding}`}\n${result}`; // baseOfTree - 2 * padding - кол-во звездочек на текущем уровне
+    for(let padding = 0; padding < height - trunkHeight; ++padding) {
+        result = `${level `${"*"}${baseOfTree - 2 * padding}${padding}`}\n${result}`;
     }
     
     return result;
