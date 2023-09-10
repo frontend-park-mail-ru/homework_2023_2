871cb285eea62c379d0381de0006c385bae25bf8
Polina <stepolant@gmail.com>
2023-09-10 22:22:47 +0300

    added exceptions

--- source/format.js
+++ source/format.js
@@ -1,7 +1,13 @@
 'use strict';
 
 const format = (numbers, columns) => {
-    if(columns<0){
+    if(Array.isArray(numbers)===false){
+        throw new TypeError(`A ${typeof(numbers)} was passed as input instead of array of numbers`)
+    }
+    if(typeof(columns)!=="number"){
+        throw new TypeError(`A ${typeof(columns)} was passed as input instead of number of columns`)
+    }
+    if(columns<=0){
         return ''
     }
     let maxNumInColumns = [];
@@ -9,6 +15,9 @@ const format = (numbers, columns) => {
     maxNumInColumns.fill(0);
     let result = '';
     numbers.forEach(function(item, i, numbers) {
+        if(typeof(item)!=="number"){
+            throw new TypeError(`An array of ${typeof(item)}s was passed as input instead of array of numbers`)
+        }
         if(String(item).length>maxNumInColumns[i%columns]){
             maxNumInColumns[i%columns] = String(item).length;
         }
