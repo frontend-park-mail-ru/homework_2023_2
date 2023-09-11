8c95abd7c104b6aac0cc410f5f5ccf4b58f6239a
Mike WInfield <lolwut-lol@yandex.ru>
2023-09-11 19:54:06 +0300

    Separated tests for TypeError, added test messages

--- tests/rle.js
+++ tests/rle.js
@@ -7,10 +7,14 @@ QUnit.module('Тестируем функцию rle', function () {
 		assert.strictEqual(rle('AVVVBBBVVXDHJFFFFDDDDDDHAAAAJJJDDSLSSSDDDD'), 'AV3B3V2XDHJF4D6HA4J3D2SLS3D4');
 		assert.strictEqual(rle('ABCDE'), 'ABCDE');
 		assert.strictEqual(rle(''), '');
-		assert.throws(() => {rle(1)});
-		assert.throws(() => {rle(['ABCDE'])});
-		assert.throws(() => {rle(null)});
-		assert.throws(() => {rle(undefined)});
-		assert.throws(() => {rle(true)});
+	});
+	QUnit.test('Ошибки при неправильном типе данных', function (assert) {
+		assert.throws(() => {rle(1)}, TypeError, 'number');
+		assert.throws(() => {rle(['ABCDE'])}, TypeError, 'object');
+		assert.throws(() => {rle(null)}, TypeError, 'null');
+		assert.throws(() => {rle(undefined)}, TypeError, 'undefined');
+		assert.throws(() => {rle(true)}, TypeError, 'boolean');
+		assert.throws(() => {rle(NaN)}, TypeError, 'NaN');
+		assert.throws(() => {rle(Infinity)}, TypeError, 'Infinity');
 	});
 });
