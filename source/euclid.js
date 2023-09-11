3b98bb62ac094fa5e65988dadcf409cfd07e552c
@gnatenko99 <tanya-g99@ya.ru>
2023-09-11 19:19:19 +0300

    Добавлены возвращение ошибок и документация

--- source/euclid.js
+++ source/euclid.js
@@ -1,12 +1,22 @@
 "use strict";
 
+/**
+ * Вычисление наибольшего общего делителя для переданных аргументов
+ * @param {...number} numbers - Числа, для которых нужно вычислить наибольший общий делитель
+ * @returns {number} - Наибольший общий делитель переданных чисел
+ * @throws {TypeError} - Аргумент должен быть последовательнстью натуральных чисел
+ */
+
 const euclid = (...numbers) => {
-	let nod = 0;
-	numbers.forEach((num) => {
+
+	if (!numbers.every((arg) => Number.isInteger(arg) && arg > 0 && arg !== Number.MAX_VALUE)) {
+		throw new TypeError("expected sequence of natural numbers");
+	}
+
+	return numbers.reduce((nod, num) => {
 		while (num && nod) {
 			num >= nod ? (num %= nod) : (nod %= num);
 		}
-		nod = num || nod;
+		return num || nod;
 	});
-	return nod;
 };
--- tests/euclid.js
+++ tests/euclid.js
@@ -1,46 +1,33 @@
-"use strict";
+'use strict';
 
-QUnit.module("Тестируем функцию euclid", function () {
-	QUnit.test("На входе всего одно число", function (assert) {
-		assert.strictEqual(euclid(1), 1, "euclid(1) === 1");
-		assert.strictEqual(euclid(2), 2, "euclid(2) === 2");
-		assert.strictEqual(euclid(7), 7, "euclid(7) === 7");
-		assert.strictEqual(euclid(6006), 6006, "euclid(6006) === 6006");
+QUnit.module('Тестируем функцию euclid', function () {
+	QUnit.test('На входе всего одно число', function (assert) {
+		assert.strictEqual(euclid(1), 1, 'euclid(1) === 1');
+		assert.strictEqual(euclid(2), 2, 'euclid(2) === 2');
+		assert.strictEqual(euclid(7), 7, 'euclid(7) === 7');
+		assert.strictEqual(euclid(6006), 6006, 'euclid(6006) === 6006');
 	});
 
-	QUnit.test(
-		"Функция должна правильно находить НОД трёх натуральных чисел",
-		function (assert) {
-			assert.strictEqual(euclid(1, 1, 1), 1, "euclid(1, 1, 1) === 1");
-			assert.strictEqual(euclid(2, 2, 2), 2, "euclid(2, 2, 2) === 2");
-			assert.strictEqual(euclid(13, 13, 26), 13, "euclid(13, 13, 26) === 13");
-			assert.strictEqual(euclid(3, 7, 1), 1, "euclid(3, 7, 1) === 1");
-			assert.strictEqual(euclid(7, 7, 13), 1, "euclid(7, 7, 13) === 1");
-			assert.strictEqual(euclid(2, 14, 16), 2, "euclid(2, 14, 16) === 2");
-			assert.strictEqual(euclid(7, 14, 21), 7, "euclid(7, 14, 21) === 7");
-			assert.strictEqual(
-				euclid(6006, 3738735, 51051),
-				3003,
-				"euclid(6006, 3738735, 51051) === 3003"
-			);
-		}
-	);
+	QUnit.test('Функция должна правильно находить НОД трёх натуральных чисел', function (assert) {
+		assert.strictEqual(euclid(1, 1, 1), 1, 'euclid(1, 1, 1) === 1');
+		assert.strictEqual(euclid(2, 2, 2), 2, 'euclid(2, 2, 2) === 2');
+		assert.strictEqual(euclid(13, 13, 26), 13, 'euclid(13, 13, 26) === 13');
+		assert.strictEqual(euclid(3, 7, 1), 1, 'euclid(3, 7, 1) === 1');
+		assert.strictEqual(euclid(7, 7, 13), 1, 'euclid(7, 7, 13) === 1');
+		assert.strictEqual(euclid(2, 14, 16), 2, 'euclid(2, 14, 16) === 2');
+		assert.strictEqual(euclid(7, 14, 21), 7, 'euclid(7, 14, 21) === 7');
+		assert.strictEqual(euclid(6006, 3738735, 51051), 3003, 'euclid(6006, 3738735, 51051) === 3003');
+	});
 
-	QUnit.test(
-		"Функция должна правильно работать с любым количеством аргументов",
-		function (assert) {
-			assert.strictEqual(euclid(1, 1, 1, 1, 1, 1, 1), 1);
+	QUnit.test('Функция должна правильно работать с любым количеством аргументов', function (assert) {
+		assert.strictEqual(euclid(1, 1, 1, 1, 1, 1, 1), 1);
 
-			const n = 17;
-			assert.strictEqual(euclid(3 * n, 2 * n, 4 * n, 7 * n, n, 21 * n), n);
+		const n = 17;
+		assert.strictEqual(euclid(3 * n, 2 * n, 4 * n, 7 * n, n, 21 * n), n);
 
-			const temp = [80325, 55275, 8746650, 3000000, 45672375, 225, 54675];
-			assert.strictEqual(
-				euclid(...[...temp, ...temp, ...temp, ...temp, ...temp]),
-				euclid(...temp)
-			);
-		}
-	);
+		const temp = [ 80325, 55275, 8746650, 3000000, 45672375, 225, 54675 ];
+		assert.strictEqual(euclid(...[ ...temp, ...temp, ...temp, ...temp, ...temp ]), euclid(...temp));
+	});
 
 	QUnit.test(
 		"Функция должна правильно работать с одинаковыми числами",
@@ -64,6 +51,53 @@ QUnit.module("Тестируем функцию euclid", function () {
 			const temp = [2, 3, 5, 7, 11, 13, 17, 19, 23];
 			assert.strictEqual(euclid(...temp), 1);
 			assert.strictEqual(euclid(...temp.map((num) => num * num)), 1);
+		}
+	);
+
+	QUnit.test(
+		"На входе массив",
+		function (assert) {
+			assert.throws(() => euclid([1, 2, 3]), new TypeError("expected sequence of natural numbers"))
+			assert.throws(() => euclid(20, 30, [1, 2, 3]), new TypeError("expected sequence of natural numbers"))
+		}
+	);
+
+	QUnit.test(
+		"На входе не натуральное число",
+		function (assert) {
+			assert.throws(() => euclid(2.4, 24, 2), new TypeError("expected sequence of natural numbers"))
+			assert.throws(() => euclid(-20, 15, 10), new TypeError("expected sequence of natural numbers"))
+			assert.throws(() => euclid(0, 1, 2, 3), new TypeError("expected sequence of natural numbers"))
+			assert.throws(() => euclid(1000, Number.POSITIVE_INFINITY), new TypeError("expected sequence of natural numbers"))
+		}
+	);
+
+	QUnit.test(
+		"На входе строка",
+		function (assert) {
+			assert.throws(() => euclid("23", 23), new TypeError("expected sequence of natural numbers"))
+		}
+	);
+
+	QUnit.test(
+		"На входе функция",
+		function (assert) {
+			assert.throws(() => euclid(4, function(){}), new TypeError("expected sequence of natural numbers"))
+		}
+	);
+
+	QUnit.test(
+		"На входе null",
+		function (assert) {
+			assert.throws(() => euclid(3, 5, null, 8), new TypeError("expected sequence of natural numbers"))
+		}
+	);
+
+	QUnit.test(
+		"На входе boolean",
+		function (assert) {
+			assert.throws(() => euclid(true), new TypeError("expected sequence of natural numbers"))
+			assert.throws(() => euclid(false), new TypeError("expected sequence of natural numbers"))
 		}
 	);
 });
