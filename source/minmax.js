1cd99e2ce779912a137dbd4da015ae57caac84b5
nikita <nikita.borisenckow@yandex.com>
2023-09-11 18:55:59 +0300

    small fixes 2

--- source/minmax.js
+++ source/minmax.js
@@ -1,25 +1,22 @@
 "use strict";
 
 /**
- * Эта функция возвращает массив чисел из переданной ей строки, игнорируя текст.
- * @param {str} str - Строка
- * @returns {arrToRet[]} - массив чисел Float
+ * @param {String} str - Строка
+ * @returns {(number)[]} - массив чисел Float
  * */
 const getNumbersArr = (str) => {
   const strs = str.split(" ");
-  const arrToRet = strs.map((el) => parseFloat(el)).filter((el) => !isNaN(el));
-  return arrToRet;
+  const arrToRetDirty = strs.map((el) => parseFloat(el));
+  const arrToRetCleaned = arrToRetDirty.filter((el) => !isNaN(el));
+  return arrToRetCleaned;
 };
 
 /**
- * Функция находит минимальное и максимальное число в строке чисел.
- *
- * @param {string} numbers - Строка чисел, разделенных пробелами.
- * @returns {number[]} Массив, с минимальным и максимальным числами.
- * Если строка не содержит чисел - возвращается [undefined, undefined].
+ * @param {String} numbers - Строка чисел, разделенных пробелами.
+ * @returns {(number|undefined)[]} Массив, с минимальным и максимальным числами
  */
 const minmax = (numbers) => {
-  if (typeof numbers != "string") {
+  if (typeof numbers !== "string") {
     return [undefined, undefined];  
   }
 
@@ -29,11 +26,10 @@ const minmax = (numbers) => {
   }
 
   const result = numbersArr.reduce(
-    (acc, curVal) => {
-      acc[0] = curVal < acc[0] ? curVal : acc[0];
-      acc[1] = curVal > acc[1] ? curVal : acc[1];
-      return acc;
-    }, [Number.POSITIVE_INFINITY, Number.NEGATIVE_INFINITY]);
+    (acc, curVal) => [
+      curVal < acc[0] ? curVal : acc[0],
+      curVal > acc[1] ? curVal : acc[1],
+    ], [Infinity, -Infinity]);
 
   return result;
   
