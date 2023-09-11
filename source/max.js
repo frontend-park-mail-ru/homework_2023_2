b67cc50b29e457f20bd95c252d11b5ca7d41159a
Umalat <dukuev037@mail.ru>
2023-09-11 14:16:30 +0300

    Fixed mistakes

--- source/max.js
+++ source/max.js
@@ -3,18 +3,16 @@
 const max = numbers => Math.max(...numbers);
 
 const inverse = (array, ind) => {
-let numbers = array.slice();
-	if (ind) {
-		const len = numbers.length;
-		let subarray1 = numbers.slice(ind)
-		let subarray2 = numbers.slice(0, ind)
-		if (ind >= 0){
-			return subarray2.concat(subarray1.reverse());
-		} else {
-			return subarray2.reverse().concat(subarray1);
-		}
-	}
-	else {
+	const numbers = array.slice();
+	if (!ind) {
 		return numbers.reverse();
 	}
+	let subarray1 = numbers.slice(ind)
+	let subarray2 = numbers.slice(0, ind)
+	if (ind >= 0){
+		subarray1.reverse();
+	} else {
+		subarray2.reverse();
+	}
+	return subarray2.concat(subarray1);
 };
