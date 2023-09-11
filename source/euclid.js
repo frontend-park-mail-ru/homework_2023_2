8a1c30a063698a7027d7e6b3dbb9a3f52a3c9c11
Islam <109366718+JuFnd@users.noreply.github.com>
2023-09-12 00:21:04 +0300

    new fix

--- source/euclid.js
+++ source/euclid.js
@@ -3,15 +3,16 @@
 /**
  * Вычисляет наибольший общий делитель для переданных аргументов.
  * @param {...number} args - Числа, для которых нужно вычислить наибольший общий делитель.
- * @returns {number|null} - Наибольший общий делитель переданных чисел или null, если переданы некорректные аргументы.
+ * @returns {number|Error} - Наибольший общий делитель переданных чисел или null, если переданы некорректные аргументы.
  */
 const euclid = (...args) => {
-  if (!args.every((arg) => typeof arg === 'number' && !isNaN(arg) && arg !== Infinity && arg !== Number.MAX_VALUE)) {
-    return null;
+  if (args.length === 0) {
+    throw new Error('Not correct args.');
   }
   
-  if (args.length === 0) {
-    return null;
+  if (!args.every((arg) => typeof arg === 'number' && !isNaN(arg) && isFinite(arg) && arg !== Number.MAX_VALUE)) {
+    throw new Error('Not correct args.');
+
   }
   
   const gcd = args.slice(1).reduce((acc, num) => calculateGCD(acc, num), args[0]);
@@ -23,14 +24,15 @@ const euclid = (...args) => {
 
 /**
 * Вычисляет наибольший общий делитель для двух чисел.
-* @param {number} a - Первое число.
-* @param {number} b - Второе число.
+* @param {number} firstNumber - Первое число.
+* @param {number} secondNumber - Второе число.
 * @returns {number} - Наибольший общий делитель для двух чисел.
  */
-const calculateGCD = (a, b) => {
-  while (b !== 0) {
-    [a, b] = [b, a % b];
+const calculateGCD = (firstNumber, secondNumber) => {
+  while (secondNumber !== 0) {
+    [firstNumber, secondNumber] = [secondNumber, firstNumber % secondNumber];
   }
     
-  return a;
+  return firstNumber;
 };
+
