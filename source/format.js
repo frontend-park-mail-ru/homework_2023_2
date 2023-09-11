9fc90136e3588c71d8363025e040644a158f8c17
Polina <stepolant@gmail.com>
2023-09-11 16:18:05 +0300

    added tests for exception situations

--- source/format.js
+++ source/format.js
@@ -1,34 +1,28 @@
 'use strict';
 
 const format = (numbers, columns) => {
-    if(Array.isArray(numbers)===false){
+    if(Array.isArray(numbers) === false){
         throw new TypeError(`A ${typeof(numbers)} was passed as input instead of array of numbers`)
     }
-    if(typeof(columns)!=="number"){
+    if(typeof(columns) !== "number"){
         throw new TypeError(`A ${typeof(columns)} was passed as input instead of number of columns`)
     }
-    if(columns<=0){
+    if(columns <= 0){
         return ''
     }
-    let maxNumInColumns = [];
-    maxNumInColumns.length=columns;
-    maxNumInColumns.fill(0);
+    const maxNumInColumns = new Array(columns).fill(0)
     let result = '';
-    numbers.forEach(function(item, i, numbers) {
-        if(typeof(item)!=="number"){
+    numbers.forEach((item, i, numbers) => {
+        if(typeof(item) !== "number"){
             throw new TypeError(`An array of ${typeof(item)}s was passed as input instead of array of numbers`)
         }
-        if(String(item).length>maxNumInColumns[i%columns]){
-            maxNumInColumns[i%columns] = String(item).length;
+        if(String(item).length > maxNumInColumns[i % columns]){
+            maxNumInColumns[i % columns] = String(item).length;
         }
     });
-    numbers.forEach(function(item, i, numbers) {
-        result+=' '.repeat(maxNumInColumns[i%columns]-String(item).length)+item;
-        if((i+1)%columns===0){
-            result+='\n';
-        }else{
-            result+=' ';
-        }
+    numbers.forEach((item, i, numbers) => {
+        result += ' '.repeat(maxNumInColumns[i % columns] - String(item).length) + item;
+        result += (i + 1) % columns === 0 ? '\n' : ' ';
     });
     return result.slice(0, -1);
 };
--- tests/format.js
+++ tests/format.js
@@ -81,6 +81,25 @@ QUnit.module('Тестируем функцию format', function () {
 	
 		assert.strictEqual(format(input, 3), expected);
 	});
+
+	QUnit.test('format работает правильно c некорректными входными данными (вместо массива чисел передана строка)', function (assert) {
+		const input = "qwertyu";
+
+		assert.throws(()=>format(input, 3), new TypeError("A string was passed as input instead of array of numbers"));
+	});
+
+	QUnit.test('format работает правильно c некорректными входными данными (вместо количества столбцов передан объект)', function (assert) {
+		const inputArray = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+		const inputCol = new Object();
+
+		assert.throws(()=>format(inputArray, inputCol), new TypeError("A object was passed as input instead of number of columns"));
+	});
+
+	QUnit.test('format работает правильно c некорректными входными данными (вместо массива чисел передан массив строк)', function (assert) {
+		const inputArray = ['10', '2', '300', '4'];
+
+		assert.throws(()=>format(inputArray, 2), new TypeError("An array of strings was passed as input instead of array of numbers"));
+	});
 });
 
 
