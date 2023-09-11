12a736d29f879c06f4aec1b316b0b9fb02b127a9
Umalat <dukuev037@mail.ru>
2023-09-12 02:01:54 +0300

    Fixed mistakes

--- source/inverse.js
+++ source/inverse.js
@@ -6,24 +6,16 @@
  * @param {number} index индекс, от(до) которого идет инверсия
  * @returns {array} возвращаемый массив
  */
-const inverse = (array, index) => {
-
+const inverse = (array, index = 0) => {
     if (!Array.isArray(array)) {
         throw new Error('Parameter is not an array!');
     } 
-	const numbers = array.slice();
-	if (typeof(index) === "undefined") {
-		return numbers.reverse();
-	}
     if (typeof index !== "number") {
         throw new TypeError('Parameter is not a number!');
     }   
-	const subarray1 = numbers.slice(index)
-	const subarray2 = numbers.slice(0, index)
-	if (index >= 0){
-		subarray1.reverse();
-	} else {
-		subarray2.reverse();
-	}
-	return subarray2.concat(subarray1);
+    const numbers = array.slice();
+    const subarray1 = numbers.slice(index)
+    const subarray2 = numbers.slice(0, index)
+    index >= 0 ? subarray1.reverse() : subarray2.reverse();
+    return subarray2.concat(subarray1);
 };
