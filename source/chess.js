d96b1e43e7ca0e52bfaf6008fcd1a5cdd19055ea
George Bezrukov <domnakolesaxxx@gmail.com>
2023-09-13 03:44:50 +0300

    fix chess.js

--- source/chess.js
+++ source/chess.js
@@ -1,29 +1,34 @@
 'use strict'
 
+const ODD_FIELD = '*';
+const EVEN_FIELD = ' ';
+const MAX_STRING_LENGTH = 2 ** 28 - 16; // Для Chrome x32, https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/length
+
 /**
  * Функция для получения шахматной доски размером NxN
  * @param size сторона доски N
  * @returns {string|null} возвращает доску в виде строки, либо null в случае неверно переданного параметра
  */
-let chess = size => {
+const chess = size => {
     if (isNaN(size)) {
-        console.log("Ожидалось число")
-        return null
+        throw new TypeError("Ожидалось число");
     }
-    if (size % 1 !== 0) {
-        console.log("Ожидалось целое число")
-        return null
+    if (size % 1) {
+        throw new TypeError("Ожидалось целое число");
     }
     if (size < 2) {
-        console.log("Не бывает доски с таким размером")
-        return null
+        throw new RangeError("Не бывает доски с таким размером");
     }
-    let desk = ""
-    for (let i = 0; i < size; i++) {
-        for (let j = 0; j < size; j++) {
-            desk += (i+j) % 2 === 0 ? '*' : ' '
-        }
-        desk = desk + '\n'
+    if (size > Math.sqrt(MAX_STRING_LENGTH) - 1) {
+        throw new RangeError("Размер доски превышает максимально возможный размер строки");
     }
-    return desk
+    let oddLine = (ODD_FIELD + EVEN_FIELD).repeat(size / 2);
+    oddLine += size % 2 === 0 ? "\n" : ODD_FIELD + '\n';
+    let evenLine = (EVEN_FIELD + ODD_FIELD).repeat(size / 2);
+    evenLine += size % 2 === 0 ? "\n" : EVEN_FIELD + '\n';
+    let desk = (oddLine + evenLine).repeat(size / 2);
+    if (size % 2 === 1) {
+        desk += oddLine;
+    }
+    return desk;
 }
--- tests/chess.js
+++ tests/chess.js
@@ -1,83 +1,87 @@
 'use strict';
 
 QUnit.module('Тестируем функцию chess', function () {
-	QUnit.test('Шахматной доски 1 на 1 не бывает', function (assert) {
-		assert.strictEqual(chess(1), null);
-		assert.strictEqual(chess('1'), null);
-	});
+    QUnit.test('Шахматной доски 1 на 1 не бывает', function (assert) {
+        assert.throws(() => chess(1), new RangeError("Не бывает доски с таким размером"));
+        assert.throws(() => chess('1'), new RangeError("Не бывает доски с таким размером"));
+    });
 
-	QUnit.test('Шахматной доски с отрицательными сторонами не бывает', function (assert) {
-		assert.strictEqual(chess(-10), null);
-		assert.strictEqual(chess('-10'), null);
-	});
+    QUnit.test('Шахматной доски с отрицательными сторонами не бывает', function (assert) {
+        assert.throws(() => chess(-10), new RangeError("Не бывает доски с таким размером"));
+        assert.throws(() => chess('-10'), new RangeError("Не бывает доски с таким размером"));
+    });
 
-	QUnit.test('Шахматной доски с нецелыми сторонами не бывает', function (assert) {
-		assert.strictEqual(chess(1.5), null);
-		assert.strictEqual(chess('1.5'), null);
-	});
+    QUnit.test('Шахматной доски с нецелыми сторонами не бывает', function (assert) {
+        assert.throws(() => chess(1.5), new TypeError('Ожидалось целое число'));
+        assert.throws(() => chess('1.5'), new TypeError('Ожидалось целое число'));
+    });
 
-	QUnit.test('Нельзя передать слово вместо размера', function (assert) {
-		assert.strictEqual(chess('abcd'), null);
-		assert.strictEqual(chess('10abcd'), null);
-	});
+    QUnit.test('Нельзя передать слово вместо размера', function (assert) {
+        assert.throws(() => chess('abcd'), new TypeError('Ожидалось число'));
+        assert.throws(() => chess('10abcd'), new TypeError('Ожидалось число'));
+    });
 
-	QUnit.test('Нельзя передать пустую строчку вместо размера', function (assert) {
-		assert.strictEqual(chess(' '), null);
-	});
+    //пустая строка кастится как 0, поэтому проходит проверку на isNan
+    QUnit.test('Нельзя передать пустую строчку вместо размера', function (assert) {
+        assert.throws(() => chess(' '), new RangeError("Не бывает доски с таким размером"));
+    });
 
-	QUnit.test('Нельзя передать объект вместо размера', function (assert) {
-		assert.strictEqual(chess(Math), null);
-	});
+    QUnit.test('Нельзя передать объект вместо размера', function (assert) {
+        assert.throws(() => chess(Math), new TypeError('Ожидалось число'));
+    });
 
-	QUnit.test('Нельзя передать bool вместо размера', function (assert) {
-		assert.strictEqual(chess(true), null);
-	});
+    //true кастится как 1, поэтому проходит проверку на isNan
+    QUnit.test('Нельзя передать bool вместо размера', function (assert) {
+        assert.throws(() => chess(true), new RangeError("Не бывает доски с таким размером"));
+    });
 
-	QUnit.test('Нельзя передать null вместо размера', function (assert) {
-		assert.strictEqual(chess(null), null);
-	});
+    //null кастится как 0, поэтому проходит проверку на isNan
+    QUnit.test('Нельзя передать null вместо размера', function (assert) {
+        assert.throws(() => chess(null), new RangeError('Не бывает доски с таким размером'));
+    });
 
-	QUnit.test('Нельзя передать undefined вместо размера', function (assert) {
-		assert.strictEqual(chess(undefined), null);
-	});
+    QUnit.test('Нельзя передать undefined вместо размера', function (assert) {
+        assert.throws(() => chess(undefined), new TypeError('Ожидалось число'));
+    });
 
-	QUnit.test('Нельзя передать NaN вместо размера', function (assert) {
-		assert.strictEqual(chess(NaN), null);
-	});
+    QUnit.test('Нельзя передать NaN вместо размера', function (assert) {
+        assert.throws(() => chess(NaN), new TypeError('Ожидалось число'));
+    });
 
-	QUnit.test('Шахматной доски бесконечного размера не бывает', function (assert) {
-		assert.strictEqual(chess(Infinity), null);
-	});
+    QUnit.test('Нельзя передать слишком большое значение', function (assert) {
+        assert.throws(() => chess(123456789087654321), new RangeError("Размер доски превышает максимально возможный размер строки"));
+        assert.throws(() => chess(Infinity), new RangeError("Размер доски превышает максимально возможный размер строки"));
+    });
 
-	QUnit.test('Шахматная доска 2 на 2', function (assert) {
-		const expected =
-			'* \n' +
-			' *\n';
-		assert.strictEqual(chess(2), expected);
-		assert.strictEqual(chess('2'), expected);
-	});
+    QUnit.test('Шахматная доска 2 на 2', function (assert) {
+        const expected =
+            '* \n' +
+            ' *\n';
+        assert.strictEqual(chess(2), expected);
+        assert.strictEqual(chess('2'), expected);
+    });
 
-	QUnit.test('Шахматная доска 3 на 3', function (assert) {
-		const expected =
-			'* *\n' +
-			' * \n' +
-			'* *\n';
-		assert.strictEqual(chess(3), expected);
-		assert.strictEqual(chess('3'), expected);
-	});
+    QUnit.test('Шахматная доска 3 на 3', function (assert) {
+        const expected =
+            '* *\n' +
+            ' * \n' +
+            '* *\n';
+        assert.strictEqual(chess(3), expected);
+        assert.strictEqual(chess('3'), expected);
+    });
 
-	QUnit.test('Шахматная доска 8 на 8', function (assert) {
-		const expected =
-			'* * * * \n' +
-			' * * * *\n' +
-			'* * * * \n' +
-			' * * * *\n' +
-			'* * * * \n' +
-			' * * * *\n' +
-			'* * * * \n' +
-			' * * * *\n';
-		assert.strictEqual(chess(8), expected);
-		assert.strictEqual(chess('8'), expected);
-	});
+    QUnit.test('Шахматная доска 8 на 8', function (assert) {
+        const expected =
+            '* * * * \n' +
+            ' * * * *\n' +
+            '* * * * \n' +
+            ' * * * *\n' +
+            '* * * * \n' +
+            ' * * * *\n' +
+            '* * * * \n' +
+            ' * * * *\n';
+        assert.strictEqual(chess(8), expected);
+        assert.strictEqual(chess('8'), expected);
+    });
 
 });
