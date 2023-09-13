319c33405dafb1f04458568c3cda26ee75a20b3c
HallowAz <amigo_honor@mail.ru>
2023-09-13 21:49:19 +0300

    let заменен на const

--- source/zip.js
+++ source/zip.js
@@ -24,7 +24,7 @@ const zip = (...objects) => {
         }
     }
 
-    let resObj = objects.reduce((props, object) => {
+    const resObj = objects.reduce((props, object) => {
         
         for (const key of Object.keys(object)) {
             
