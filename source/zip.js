6c45fde0dc3124c0cd3a3637fe828be6e9b68ea4
HallowAz <amigo_honor@mail.ru>
2023-09-13 19:29:53 +0300

    Заменил let на const

--- source/zip.js
+++ source/zip.js
@@ -6,7 +6,7 @@ let zip = (...objects) => {
         return {};  
     }
 
-    for (let object of objects) {
+    for (const object of objects) {
         if (typeof object != 'object') {
             throw new Error('Были переданы данные, содержащие не только объекты');
         }
@@ -14,7 +14,7 @@ let zip = (...objects) => {
 
     let resObj = objects.reduce((props, object) => {
         
-        for (let key of Object.keys(object)) {
+        for (const key of Object.keys(object)) {
             if (!(props.hasOwnProperty(key)))
                 props[key] = object[key];
         }
