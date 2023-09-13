361bed40390468c870c9cd44791779b8da2c04f7
alfa <alfaiv@yandex.ru>
2023-09-13 07:33:36 +0300

    fifth rev

--- source/solve.js
+++ source/solve.js
@@ -1,17 +1,11 @@
 'use strict'
-
 /**
  * @param {string} equation - Исходное выражение
  * @param {number} x_value - Значение для подстановки в x
  * @description Подставновка в выражение с x, соответсвующего значени
  * @returns {number} Вычисленное значение после подставновки x
  */
-
-
 const solve = function (equation, x_value) {
-    
-    let solution = null;
-    equation = equation.replaceAll('x', String(x_value));
-    solution =  eval(equation);
-    return solution;
+    solution = equation.replaceAll('x', String(x_value));
+    return eval(solution);
 };
