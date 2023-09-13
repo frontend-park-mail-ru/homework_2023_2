d8341f057a2d62c41d6dd45ecea8347f66b3c088
alfa <alfaiv@yandex.ru>
2023-09-13 20:56:19 +0300

    sixth rev

--- source/solve.js
+++ source/solve.js
@@ -6,6 +6,6 @@
  * @returns {number} Вычисленное значение после подставновки x
  */
 const solve = function (equation, x_value) {
-    solution = equation.replaceAll('x', String(x_value));
-    return eval(solution);
+    const replacing_x_to_digit = equation.replaceAll('x', String(x_value));
+    return eval(replacing_x_to_digit);
 };
