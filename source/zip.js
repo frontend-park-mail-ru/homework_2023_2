67a4db2a715bb5196876c079db5b21e896575158
HallowAz <amigo_honor@mail.ru>
2023-09-13 18:42:11 +0300

    Добавлена проверка входных параметров на соотвествие типу object

--- source/zip.js
+++ source/zip.js
@@ -2,6 +2,12 @@
 
 let zip = (...objects) => {
 
+    for (let object of objects) {
+        if (typeof object != 'object') {
+            throw new Error('Были переданы данные, содержащие не только объекты');
+        }
+    }
+
     let resObj = {};
 
     objects.forEach( object => {
