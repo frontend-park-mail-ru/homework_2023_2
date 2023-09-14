d624fdaf9747ec5a5c04056e56a6e9c3ac8331d5
Tanya <tanyaemelyanova2015@gmail.com>
2023-09-14 16:10:04 +0300

    add condition and codestyle

--- source/zip.js
+++ source/zip.js
@@ -2,14 +2,22 @@
 
 const zip = (...objects) => {
     objects.map((object) => {
-        if (Array.isArray(object) || typeof object !== 'object' || object === null || object instanceof String)
+        if (
+            Array.isArray(object) 
+            || typeof object !== 'object' 
+            || object === null 
+            || object instanceof String 
+            || object instanceof Number
+        ) {
             throw new TypeError('TYPE ERROR IN ARGUMENTS')
+        }
     })
     const resultObject = {}
     objects.map((object) => {
         Object.entries(object).forEach(([key, value]) => {
-            if (!resultObject.hasOwnProperty(key))
+            if (!resultObject.hasOwnProperty(key)) {
                 resultObject[key] = value
+            }
         })
     })
     return resultObject
