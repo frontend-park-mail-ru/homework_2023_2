b2268a80efc704efde8fa11ba59e1bc8687add41
1qwaka <yalenin01@gmail.com>
2023-09-14 12:47:39 +0300

    minmax code style fix

--- source/minmax.js
+++ source/minmax.js
@@ -7,12 +7,16 @@
  *                    первый элемент - максимальное 
  */
 function minmax(str) {
-    if (!(typeof str === 'string' || str instanceof String)) throw TypeError("str argument should be a String");
-    
+    if (!(typeof str === 'string' || str instanceof String)) {
+        throw TypeError('str argument should be a String');
+    }
+
     const minAndMax = [ undefined, undefined ];
     const numberStrings = str.match(/[+-]?((\d+([.]\d*)?([eE][+-]?\d+)?|[.]\d+([eE][+-]?\d+)?)|Infinity)/gu);
 
-    if (!numberStrings) return minAndMax;
+    if (!numberStrings) {
+        return minAndMax;
+    }
 
     const numbers = numberStrings.reduce((acc, cur) => {
         if (cur.length > 0) {
@@ -24,7 +28,9 @@ function minmax(str) {
         return acc;
     }, []);
 
-    if (!numbers.length) return minAndMax;
+    if (!numbers.length) {
+        return minAndMax;
+    }
 
     minAndMax[0] = minAndMax[1] = numbers[0];
 
