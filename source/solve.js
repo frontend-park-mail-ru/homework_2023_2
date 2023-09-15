aa7f452a0f3c261b5bc8b9231210b73c5f4400e7
alfa <alfaiv@yandex.ru>
2023-09-15 19:34:33 +0300

    switch to camelCase

--- source/solve.js
+++ source/solve.js
@@ -1,15 +1,15 @@
 'use strict'
 /**
  * @param {string} equation - Исходное выражение
- * @param {number} x_value - Значение для подстановки в x
+ * @param {number} xValue - Значение для подстановки в x
  * @description Подставновка в выражение с x, соответсвующего значени
  * @returns {number} Вычисленное значение после подставновки x
  * @throws {TypeError} Проверка типа входных данных
  * @throws {Error} Проверка выражения
  * @throws {Error} Проверка наличиия x в выражении
  */
-const solve = function (equation, x_value) {
-    if ((typeof equation !== 'string') || typeof x_value !== 'number') {
+const solve = function (equation, xValue) {
+    if ((typeof equation !== 'string') || typeof xValue !== 'number') {
         throw new TypeError('Введен некоретный тип входных данных!');
     }
     if (equation.match(/[^-+)(\/\*x\d\s]/)) {
@@ -18,6 +18,6 @@ const solve = function (equation, x_value) {
     if (!equation.match(/x/)) {
         throw new Error('В выражениее отсутсвует x!');
     }
-    const replacing_x_to_digit = equation.replaceAll('x', String(x_value));
-    return eval(replacing_x_to_digit);
+    const replacingXtoDigit = equation.replaceAll('x', String(xValue));
+    return eval(replacingXtoDigit);
 };
