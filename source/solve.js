31331133cb04d7afd7db263eeb87737ec9cf933c
alfa <alfaiv@yandex.ru>
2023-09-16 11:20:32 +0300

    add hand made eval func

--- source/solve.js
+++ source/solve.js
@@ -1,4 +1,5 @@
-'use strict'
+'use strict';
+
 /**
  * @param {string} equation - Исходное выражение
  * @param {number} xValue - Значение для подстановки в x
@@ -8,16 +9,31 @@
  * @throws {Error} Проверка выражения
  * @throws {Error} Проверка наличиия x в выражении
  */
+let calculate = function(match, equation) {
+    let mulDivRegExpr = /(-?[\d\.]+)\s*([\*\/])\s*(-?[\d\.]+)/g;
+    let plusMinusRegExpr = /(-?[\d\.]+)\s*([\+-])\s*(-?[\d\.]+)/g;
+    while(mulDivRegExpr.test(equation))
+       equation = equation.replace(mulDivRegExpr, (match, a, sign, b) => {return sign == "/" ? a / b : a * b;});
+    while(plusMinusRegExpr.test(equation))
+       equation = equation.replace(plusMinusRegExpr, (match, a, sign, b) => {return sign == "+" ? Number(a) + Number(b) : a - b;});
+    return Number(equation);
+}
+const evalHandMade = (equation) => {
+    while(equation.indexOf("(") !== -1) {
+        equation = equation.replace(/\(([^\(\)]*)\)/g, calculate);
+    }
+    return calculate("", equation);
+}
 const solve = function (equation, xValue) {
-    if ((typeof equation !== 'string') || typeof xValue !== 'number') {
+    if ((!(equation instanceof String) && typeof equation !== 'string') || typeof xValue !== 'number') {
         throw new TypeError('Введен некоретный тип входных данных!');
     }
     if (equation.match(/[^-+)(\/\*x\d\s]/)) {
         throw new Error('Введено некоретное выражение!');
     }
     if (!equation.match(/x/)) {
-        throw new Error('В выражениее отсутсвует x!');
+        throw new Error('В выражении отсутсвует x!');
     }
     const replacingXtoDigit = equation.replaceAll('x', String(xValue));
-    return eval(replacingXtoDigit);
+    return evalHandMade(replacingXtoDigit);
 };
