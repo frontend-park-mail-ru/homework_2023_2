6e01e874cc5cdcce3025f34919b1ad78c2b56a45
Spirin Mihail <misha_spirin_90@mail.ru>
2023-09-17 18:46:05 +0300

    add throw

--- source/anagram.js
+++ source/anagram.js
@@ -2,7 +2,7 @@
 
 /**
  * Deep clones array or string
- * @param {array|string} items - variable to be deep copy
+ * @param {array|string} items - variable to be deep copied
  * @returns {array|string} - a deep copy of items
  */
 
@@ -13,13 +13,14 @@ const clone = (items) => items.map(item => Array.isArray(item) ? clone(item) : i
  * Creates an object containing an amount of times each letter is met in a word
  * @param {string} word - string that we count letters in
  * @returns {Object.<string, number>} - object containing letter: number - amount of times letter is found in a word
+ * @throws {TypeError} argument must be string
  */
-function countLetters(word) {
-    if (typeof word !== "string") {
+function countLetters(wordInput) {
+    if (typeof wordInput !== "string") {
         throw new TypeError("word must be string")
     }
 
-    word = word.toLowerCase()
+    const word = wordInput.toLowerCase()
     const letterCount = {};
     for (let i = 0; i < word?.length; i++) {
         letterCount[word[i]] = letterCount[word[i]] + 1 || 1;
@@ -33,6 +34,7 @@ function countLetters(word) {
  * @param {?string} firstWord - first word
  * @param {?string} secondWord - second word
  * @returns {boolean} - true of word is an anagram, false otherwise
+ * @throws {TypeError} both arguments must be strings
  */
 function areAnagrams(firstWord, secondWord) {
     if (firstWord === null || secondWord === null)
@@ -59,6 +61,7 @@ function areAnagrams(firstWord, secondWord) {
  * Groups anagrams into arrays and returns an array of those groups
  * @param {?Array.<string>} inputWords - array of words to check for anagrams
  * @returns {?Array.<Array.<string>>} - array of anagrams grouped into arrays
+ * @throws {TypeError} argument must be a an arrays of strings
  */
 function anagram(inputWords) {
     if (inputWords === null)
