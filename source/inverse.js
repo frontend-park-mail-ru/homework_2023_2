8ebe76c9e793777682a4ab0ca310e534bc0be43c
ShelbyKS <krylov.sanches@mail.ru>
2023-09-18 17:00:20 +0300

    refactoring changes

--- source/inverse.js
+++ source/inverse.js
@@ -6,30 +6,28 @@
  * @throws {TypeError} - Если первый аргумент не является массивом или второй аргумент не является числом.
  */
 
-const inverse = (arr, num = 0) => {
-    if (!Array.isArray(arr)) {
+const inverse = (arrNums, num = 0) => {
+    if (!Array.isArray(arrNums)) {
         throw new TypeError('Первый аргумент должен быть массивом');
     }
       
-    if (!((typeof num === 'number' || num instanceof Number) && (num % 1 === 0 || Number.isInteger(num)))) {
+    if (!((typeof num === 'number' || num instanceof Number) && Number.isInteger(num))) {
         throw new TypeError('Второй аргумент должен быть целым числом');
     }
 
-    if (arr.every((value) => value === arr[0])) {
-        return arr
-    }
-
-    if(arr.length === 1) {
-        return arr
+    if (arrNums.every((value) => value === arrNums[0])) {
+        return arrNums;
     }
 
     if (num >= 0) {
-        return arr.slice(0, num).concat(arr.slice(num).reduce((accumulator, value) => {
+        const newArrNums = arrNums.slice(0, num).concat(arrNums.slice(num).reduce((accumulator, value) => {
             return [value, ...accumulator];
-        }, []))
+        }, []));
+        return newArrNums
     }
-    
-    return arr.slice(0, arr.length + num).reduce((accumulator, value) => {
+
+    const newArrNums = arrNums.slice(0, arrNums.length + num).reduce((accumulator, value) => {
         return [value, ...accumulator];
-    }, []).concat(arr.slice(num))
+    }, []).concat(arrNums.slice(num))
+    return newArrNums
 }
