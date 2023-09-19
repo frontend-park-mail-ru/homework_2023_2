72ed92a7893b71f61b829c3b1893449fe1c4c462
Spirin Mihail <misha_spirin_90@mail.ru>
2023-09-19 19:44:52 +0300

    add string object check

--- source/anagram.js
+++ source/anagram.js
@@ -5,9 +5,16 @@
  * @param {array|string} items - variable to be deep copied
  * @returns {array|string} - a deep copy of items
  */
-
 const clone = (items) => items.map(item => Array.isArray(item) ? clone(item) : item);
 
+/**
+ * Checks whether word is string
+ * @param {string} word - a word we are checking
+ * @returns {boolean} true if word is string, false if not
+ */
+const isString = (word) => {
+    return typeof word === "string" || word instanceof String
+}
 
 /**
  * Creates an object containing an amount of times each letter is met in a word
@@ -16,7 +23,7 @@ const clone = (items) => items.map(item => Array.isArray(item) ? clone(item) : i
  * @throws {TypeError} argument must be string
  */
 function countLetters(wordInput) {
-    if (typeof wordInput !== "string") {
+    if (!isString(wordInput)) {
         throw new TypeError("word must be string")
     }
 
@@ -39,7 +46,7 @@ function countLetters(wordInput) {
 function areAnagrams(firstWord, secondWord) {
     if (firstWord === null || secondWord === null)
         return false
-    if (typeof firstWord !== "string" || typeof secondWord !== "string") {
+    if (!isString(firstWord) || !isString(secondWord)) {
         throw new TypeError("word must be string")
     }
     if (firstWord?.length != secondWord?.length)
@@ -68,9 +75,9 @@ function anagram(inputWords) {
         return null
 
     if (!(Array.isArray(inputWords)
-    && inputWords.every((word) => typeof word === "string"))) {
-            throw new TypeError("argument must be an array of strings")
-        }
+        && inputWords.every((word) => isString(word)))) {
+        throw new TypeError("argument must be an array of strings")
+    }
 
     const words = clone(inputWords)
     words.sort();
