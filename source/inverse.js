ef263ed8402f936efb65ce1e4c4fe9d08046e06c
Rissenberg <114286666+rissenberg@users.noreply.github.com>
2023-09-23 23:50:00 +0300

    reverse func to const

--- source/inverse.js
+++ source/inverse.js
@@ -9,7 +9,7 @@
  * @returns {Array.<Object>} Массив с измененным порядок элементов
  * @throws {TypeError} Проверка типа входных данных. Исключение выбрасывается при неккоректном типе входных данных
  */
-let inverse = (array, offset = 0) => {
+const inverse = (array, offset = 0) => {
 	if (!Array.isArray(array)) {
 		throw new TypeError('Некоретный тип входных данных - неверно задан массив');
 	}
